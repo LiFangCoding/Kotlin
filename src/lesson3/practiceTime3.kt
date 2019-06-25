2b5ff10bf0b538486d35af6de3df1d619d60a00d
LiFangCoding <fanglihust@gmail.com>
2019-06-25 17:26:14 +0900

    completeGreeting

--- src/lesson3/practiceTime3.kt
+++ src/lesson3/practiceTime3.kt
@@ -1,9 +1,28 @@
 package lesson3
 
+import java.util.*
+
 fun main(args: Array<String>) {
-    dayOfWeek()
+//    dayOfWeek()
+    println("Hello, ${args[0]}!")
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+
 }
 
 fun dayOfWeek() {
     println("What day is it today?")
+    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println(when (day) {
+        1 -> "Sunday"
+        3 -> "Monday"
+        4 -> "Tuesday"
+        5 -> "Wednesday"
+        6 -> "Thursday"
+        7 -> "Friday"
+        else -> "Time has stopped"
+    })
 }
